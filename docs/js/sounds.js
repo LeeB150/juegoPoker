8820aa1a250a7a0721ee6baea14989eca052cb6b
LeeB150 <72904329+LeeB150@users.noreply.github.com>
2020-10-25 22:08:13 -0500

    audio cerrar pop up

--- docs/js/sounds.js
+++ docs/js/sounds.js
@@ -1,18 +1,20 @@
-// let modal = document.querySelector(".lbl-modal")
-// let audioEtiqueta = document.querySelector("audio")
-
-// modal.addEventListener("click", () => {
-//     audioEtiqueta.setAttribute("src", "../sounds/pop.mp3")
-//     audioEtiqueta.play()
-//     console.log(`Reproduciendo: ${audioEtiqueta.src}`)
-// })
-
-let modal = document.getElementById("sound")
+//sonido popUP
+// let modal = document.getElementById("sound")
 let audioEtiqueta = document.querySelector("audio")
-
 
 function mouseDown(){
     document.getElementById("sound")
     audioEtiqueta.setAttribute("src", "sounds/pop.mp3")
     audioEtiqueta.play()
+}
+
+//sonido popClose
+// let close = document-getElementById("soundCls")
+let audioEtiquet = document.querySelector("audio")
+
+function closePopUp(){
+    document.getElementById("soundCls")
+    audioEtiquet.setAttribute("src", "sounds/sonidoclick.mp3")
+    audioEtiquet.play()
+    console.log(audioEtiquet)
 }
