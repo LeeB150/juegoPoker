2a86383b7bd8f0d782cfd6d12027fd8e77ec994f
LeeB150 <72904329+LeeB150@users.noreply.github.com>
2020-10-27 16:26:07 -0500

    commit 27 del 10 del 2020

--- docs/js/moves.js
+++ docs/js/moves.js
@@ -1,16 +1,37 @@
-var imgA = document.getElementById('adelante'),
-    imgD = document.getElementById('atras');
+var btnJugar = document.getElementById('btnJugar'),
+    imgA = document.getElementsByClassName('imgCar'),
+    imgD = document.getElementsByClassName('imgReverso'),
+    card1 = document.getElementById('card1'),
+    card2 = document.getElementById('card2'),
+    card3 = document.getElementById('card3'),
+    card4 = document.getElementById('card4'),
+    card5 = document.getElementById('card5'),
+    rev1 = document.getElementById('rev1'),
+    rev2 = document.getElementById('rev2'),
+    rev3 = document.getElementById('rev3'),
+    rev4 = document.getElementById('rev4'),
+    rev5 = document.getElementById('rev5');
+
+
+// boton jugar hace aparecer a las cartas
+function cartasAparecen(){
+    for(let i=0; i<5; i++){
+        imgA[i].style.display='flex';
+    }
+}
+// --------------------------------------------------------
+
 
 function imgDelante(){
-    imgA.style.display='none';
-    imgD.style.display='flex';
-
+    card1.style.display='none';
+    rev1.style.display='flex';    
 }
 function imgAtras(){
-    imgA.style.display='flex';
-    imgD.style.display='none';
-
+    card1.style.display='flex';
+    rev1.style.display='none';
 }
+btnJugar.addEventListener('click',cartasAparecen,true)
+card1.addEventListener('click',imgDelante,true)
+rev1.addEventListener('click',imgAtras,true)
 
-imgA.addEventListener('click',imgDelante,true)
-imgD.addEventListener('click',imgAtras,true)
+
--- docs/js/sounds.js
+++ docs/js/sounds.js
@@ -1,17 +1,13 @@
-//sonido popUP
 let audioEtiqueta = document.querySelector("audio")
 
+//sonido popUP
 function mouseDown(){
-    document.getElementById("sound")
     audioEtiqueta.setAttribute("src", "sounds/pop.mp3")
     audioEtiqueta.play()
 }
 
 //sonido popClose
-let audioEtiquet = document.querySelector("audio")
-
 function closePopUp(){
-    document.getElementById("soundCls")
-    audioEtiquet.setAttribute("src", "sounds/sonidoclick.mp3")
-    audioEtiquet.play()
+    audioEtiqueta.setAttribute("src", "sounds/sonidoclick.mp3")
+    audioEtiqueta.play()
 }
